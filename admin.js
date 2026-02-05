30f4541cdc6e46aec548c8cdee51c80655730bad
fadiahaswat <andiaqillahfadiahaswat@gmail.com>
2026-02-05 15:14:11 +0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -9,7 +9,7 @@ import { initializeAppCheck, ReCaptchaV3Provider } from "https://www.gstatic.com
 // PERINGATAN KEAMANAN: API Key ini bersifat publik dan seharusnya dibatasi melalui 
 // Firebase Console > Project Settings > API restrictions untuk domain yang diizinkan
 const firebaseConfig = {
-  apiKey: "AIzaSyBH6--Yq7y3101dSVR9A9yF-ytEA4rkpuU",
+  apiKey: "AIzaSyAWPIcS8h3kE6kJYBxjeVFdSprgrMzOFo8",
   authDomain: "lazismu-auth.firebaseapp.com",
   projectId: "lazismu-auth",
   storageBucket: "lazismu-auth.firebasestorage.app",
